5f578e5719603f778f6ce7826fe81858f0c8a72f
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-09-22 22:49:39 +0300

    Initial commit

---
+++ app/src/main/java/com/ozalp/kotlintutorial/KotlinTutorial.kt
@@ -0,0 +1,13 @@
+package com.ozalp.kotlintutorial
+
+class KotlinTutorial {
+    var x = 0
+    var y = 9
+
+    constructor() {
+        println(x + y)
+
+        // JS, Dart vsdeki gibi const anahtar kelimesiyle aynı işi yapar
+        val a = 6
+    }
+}
